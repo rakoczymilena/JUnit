532f4ccd6b89804cd68d0ab65654a8ca5dbaa9c1
Milenka <Milenka@LAPTOP-FQGQS59A>
2021-07-27 20:20:32 +0100

    A JUnit test file called “MyMathTest.java” and a java file “MyMath.java”

---
+++ JUnit/src/Testing/MyMath.java
@@ -0,0 +1,20 @@
+package Testing;
+
+public class MyMath {
+	public int MyAdd(int a, int b) {
+		return a + b;
+	}
+	
+	public int MySubtract(int a, int b) {
+		return a - b;
+	}
+	
+	public int MyMultiply(int a, int b) {
+		return a * b;
+	}
+	
+	public int MyDivide(int a, int b) {
+		return a/b;
+	}
+	
+}
