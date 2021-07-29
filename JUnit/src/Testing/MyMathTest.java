701cdfdd6263f78ae7851181ce9bcebfa8c9d942
Milenka <Milenka@LAPTOP-FQGQS59A>
2021-07-29 21:56:33 +0100

    JUnit test adding the following four stub methods: •	testAdd() •	testSubtract() •	testMultiply() •	testDivide()

--- JUnit/src/Testing/MyMathTest.java
+++ JUnit/src/Testing/MyMathTest.java
@@ -7,8 +7,36 @@ import org.junit.jupiter.api.Test;
 class MyMathTest {
 
 	@Test
-	void test() {
+	public void test() {
 		fail("Not yet implemented");
 	}
 
+	@Test
+	 public void testAdd() {
+	  int result = calc.add(10, 20);
+	  assertEquals(30, result, 0);
+	 }
+
+	 @Test
+	 public void testSubtract() {
+	  int result = calc.subtract(100, 90);
+	  assertEquals(10, result, 0);
+	 }
+
+	 @Test
+	 public void testMultiply() {
+	  int result = calc.multiply(2, 100);
+	  assertEquals(200, result, 0);
+	 }
+
+	 @Test
+	 public void testDivide() {
+	  int result = calc.divide(100, 10);
+	  assertEquals(10, result, 0.001);
+	  
+	 }
+	 
 }
+
+
+
